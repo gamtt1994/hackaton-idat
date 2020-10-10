62daf0906fa2be2efb730d2e8d6a7c8db74ee57b
Gustavo Maldonado <gamtt1994@gmail.com>
2020-10-10 08:37:22 -0500

    resolución del ejercicio 4

---
+++ Ejercicio4.js
@@ -0,0 +1,23 @@
+/*
+Ejercicio #4: Creando arrays
+- Crear un array llamado *meses* y que almacene el nombre de los doce
+meses del año
+- Mostrar por pantalla los doce nombres utilizando la función *alert()*
+*/
+
+let meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+alert("Los meses del año son: " + meses);
