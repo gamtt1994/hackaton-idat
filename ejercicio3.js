4e55c3f916a6e95cfda4bda7117612a75a5d0c59
Gustavo Maldonado <gamtt1994@gmail.com>
2020-10-10 08:39:04 -0500

    resolución del ejercicio 4 html y js

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,8 +1,12 @@
 /*
+
+
 Ejercicio #3: Valor futuro de una inversión
 - Realice un programa para calcular el valor futuro de una inversión; la
 fórmula para obtener este valor es la siguiente:
 valor_futuro =valor*Math.pow (1+tasa/100,periodo)
+
+
 */
 let valor = 2000;
 let tasa = 2.5;
