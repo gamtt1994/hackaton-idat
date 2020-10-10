fb092436cb4dac160dac7bc966b79e69fd0ed39c
Gustavo Maldonado <gamtt1994@gmail.com>
2020-10-10 09:03:32 -0500

    resolución del ejercicios final

--- ejercicio0.js
+++ ejercicio0.js
@@ -5,12 +5,12 @@ devolver el menor de los valores de una la lista
 
 var arreglo = [-1, 6, -5.1, -5.11555, -5, 5, 89, 32];
 
-var maxValue = arreglo[0];
+var minValue = arreglo[0];
 for (var i = 0; i < arreglo.length; i++) {
   var currentValue = arreglo[i];
 
-  if (currentValue < maxValue) {
-    maxValue = currentValue;
+  if (currentValue < minValue) {
+    minValue = currentValue;
   }
 }
-console.log("menor numero es", maxValue);
+console.log("menor numero es", minValue);
